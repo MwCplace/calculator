aa22d3a7e3703b18ae03672f2b17d926b790f8e3
MwCplace <134137626+MwCplace@users.noreply.github.com>
2024-04-25 02:23:37 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-let output = $("#output");
+let output = document.querySelector("#output");
 let str = '';
 
-$("button").click(function() {
+document.querySelector("button").click(function() {
   switch (this.innerHTML) {
     case 'CE':
       str = '';
@@ -26,3 +26,32 @@ function result(str) {
   }
   return 0;
 }
+
+// let output = $("#output");
+// let str = '';
+
+// $("button").click(function() {
+//   switch (this.innerHTML) {
+//     case 'CE':
+//       str = '';
+//       break;
+//     case '=':
+//       str = result(str);
+//       break;
+//     default:
+//       str += this.innerHTML
+//       break;
+//   }
+//   output.innerHTML = str;
+// });
+
+// function result(str) {
+//   if (str == '') {
+//     return 0;
+//   }
+//   else {
+//   //https://stackoverflow.com/questions/6479236/calculate-string-value-in-javascript-not-using-eval
+//     return Math.round(eval(str)*1000)/1000;
+//   }
+//   return 0;
+// }
